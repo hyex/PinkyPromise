e72cb32ecd3d61068f2c4ed9e0530b36d7caf59c
hyex <hyexkim@gmail.com>
2020-01-13 13:39:43 +0900

    generate some files for team

---
+++ PinkyPromise/ViewControllers/HomeTab/HomeTabMainVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeTabMainVC.swift
+//  PinkyPromise
+//
+//  Created by kimhyeji on 1/13/20.
+//  Copyright © 2020 hyejikim. All rights reserved.
+//
+
+import UIKit
+
+class HomeTabMainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PinkyPromise/ViewControllers/MyTab/MyTabMainVC.swift
@@ -0,0 +1,30 @@
+//
+//  MyTabMainVC.swift
+//  PinkyPromise
+//
+//  Created by kimhyeji on 1/13/20.
+//  Copyright © 2020 hyejikim. All rights reserved.
+//
+
+import UIKit
+
+class MyTabMainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
