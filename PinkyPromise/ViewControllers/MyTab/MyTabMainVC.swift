af3537c6d128df1f9edc0415010258632e14e3eb
hyex <hyexkim@gmail.com>
2020-01-14 15:51:19 +0900

    add segmentControl and views(Day & Promise)

--- PinkyPromise/ViewControllers/MyTab/MyTabMainVC.swift
+++ PinkyPromise/ViewControllers/MyTab/MyTabMainVC.swift
@@ -10,12 +10,31 @@ import UIKit
 
 class MyTabMainVC: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+
+    @IBOutlet weak var dayChildView: UIView!
+    @IBOutlet weak var promiseChildView: UIView!
+    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        self.navigationController?.navigationBar.isHidden = true
+        dayChildView.isHidden = false
+        promiseChildView.isHidden = true
 
     }
     
-
+    
+    @IBAction func indexChanged(_ sender: Any) {
+        
+        switch segmentedControl.selectedSegmentIndex{
+        case 0:
+            dayChildView.isHidden = false
+            promiseChildView.isHidden = true
+        case 1:
+            dayChildView.isHidden = true
+            promiseChildView.isHidden = false
+        default:
+            break
+        }
+    }
+    
 }
