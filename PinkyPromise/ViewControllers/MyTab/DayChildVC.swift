86b7fe1499f90d79f96b3440c072553caf3e63e5
hyex <hyexkim@gmail.com>
2020-01-14 16:38:53 +0900

    add collectionview to dayChildVC

--- PinkyPromise/ViewControllers/MyTab/DayChildVC.swift
+++ PinkyPromise/ViewControllers/MyTab/DayChildVC.swift
@@ -10,22 +10,64 @@ import UIKit
 
 class DayChildVC: UIViewController {
 
+    @IBOutlet weak var dayCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        dayCollectionView.delegate = self
+        dayCollectionView.dataSource = self
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+extension DayChildVC: UICollectionViewDelegate {
+    
+}
+
+extension DayChildVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        var cell = UICollectionViewCell()
+        if let dayCell = dayCollectionView.dequeueReusableCell(withReuseIdentifier: "DayCVC", for: indexPath) as? DayCVC {
+            //            moreTableList.sort(by: {$0.promiseCount > $1.promiseCount})
+            //                    let rowData = self.moreTableList[indexPath.row]
+            
+            cell = dayCell
+        }
+        
+        return cell
+    }
+    
+}
+
+extension DayChildVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let width = 375
+//        let height =
+        let width = dayCollectionView.bounds.width
+        
+        /**
+         MARK: 이 높이를 테이블뷰의 크기에 맞추어서 하는 게 목표
+         */
+        let height = dayCollectionView.bounds.height/3
+        return CGSize(width: width, height: height)
+    }
+}
+
+
---
+++ PinkyPromise/Views/MyTab/DayCVC.swift
@@ -0,0 +1,16 @@
+//
+//  DayCVC.swift
+//  PinkyPromise
+//
+//  Created by kimhyeji on 1/14/20.
+//  Copyright © 2020 hyejikim. All rights reserved.
+//
+
+import UIKit
+
+class DayCVC: UICollectionViewCell {
+    
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var promiseTabelView: UITableView!
+    
+}
