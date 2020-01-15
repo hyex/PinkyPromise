6bf0b3504b80e6f77f3060d726ee9a8a5e2b099f
odyflame <ieehyeon@naver.com>
2020-01-15 16:02:20 +0900

    updating MyTab

---
+++ PinkyPromise/Models/MyTab/LeeHJ/LeeHJCustomMyCVC.swift
@@ -0,0 +1,49 @@
+//
+//  LeeHJCustomMyCVC.swift
+//  PinkyPromise
+//
+//  Created by apple on 2020/01/13.
+//  Copyright © 2020 hyejikim. All rights reserved.
+//
+
+import UIKit
+
+
+class LeeHJCustomMyCVC: UICollectionViewCell {
+    
+    @IBOutlet weak var appSlider: LeeHJCustomSlider!
+    @IBOutlet weak var showSliderValue: UILabel!
+    @IBOutlet weak var promiseName: UILabel!
+    
+    @IBAction func sliderValueChanged(_ sender: Any?) {
+        self.showSliderValue.text = "\(self.appSlider.value)"
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.backgroundColor = UIColor.white
+        
+        self.layer.cornerRadius = 8.0
+        
+        self.contentView.layer.cornerRadius = 8.0
+        self.contentView.layer.borderWidth = 2.0
+        self.contentView.layer.borderColor = UIColor.clear.cgColor
+        self.contentView.layer.masksToBounds = true
+        
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+        self.layer.shadowRadius = 5.0
+        self.layer.shadowOpacity = 1.0
+        self.layer.masksToBounds = false
+        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.contentView.layer.cornerRadius).cgPath
+        
+    }
+    
+//    init() {
+//        appSlider.minimumTrackTintColor = UIColor(named: "Red")
+//        appSlider.maximumTrackTintColor = UIColor(named: "DarkRed")
+//    }
+//    appSlider.mininumTrackTintColor = UIColor(named: "Red")
+//    appSlider.maximunTrackTintColor = UIColor(named: "DarkRed")
+}
