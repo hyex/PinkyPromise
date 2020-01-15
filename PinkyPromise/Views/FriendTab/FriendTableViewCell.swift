cc8138759e3c03e147b09a3ef2f81273f9be4aea
seonyoung169 <hwagae2003@gmail.com>
2020-01-15 15:43:02 +0900

    add view cell file

---
+++ PinkyPromise/Views/FriendTab/FriendTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FriendTableViewCell.swift
+//  PinkyPromise
+//
+//  Created by SEONYOUNG LEE on 2020/01/15.
+//  Copyright © 2020 hyejikim. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+    @IBOutlet weak var friendProfileImg: UIImageView!
+    
+    @IBOutlet weak var friendNameLabel: UILabel!
+}
