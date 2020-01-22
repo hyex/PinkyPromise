b981545e95c1c0baf3132544ee91f320bdcaa3e5
hyex <hyexkim@gmail.com>
2020-01-22 19:58:32 +0900

    day plus automatically and add backgroundColor

--- PinkyPromise/ViewControllers/MyTab/DayChildVC.swift
+++ PinkyPromise/ViewControllers/MyTab/DayChildVC.swift
@@ -14,7 +14,7 @@ struct Promise {
 }
 
 struct Day {
-    var day: String
+    var day: Int
     var promise: [Promise]
 }
 
@@ -22,37 +22,54 @@ class DayChildVC: UIViewController {
     
     @IBOutlet weak var dayTableView: UITableView!
     
-    
+
     var days: [Day] = [
-        Day(day: "MON  \n 1", promise: [
+        Day(day: 0, promise: [
             Promise(promiseName: "red", promiseColor: "red"),
             Promise(promiseName: "yellow", promiseColor: "yellow")
         ]),
-        Day(day: "TUE  \n 2", promise: [
+        Day(day: 1, promise: [
             Promise(promiseName: "yellow", promiseColor: "yellow"),
             Promise(promiseName: "green", promiseColor: "green"),
             Promise(promiseName: "blue", promiseColor: "blue"),
-            Promise(promiseName: "brown", promiseColor: "brown"),
+            Promise(promiseName: "purple", promiseColor: "purple"),
             Promise(promiseName: "blue", promiseColor: "blue"),
             Promise(promiseName: "green", promiseColor: "green"),
-            Promise(promiseName: "blue", promiseColor: "blue")
+            Promise(promiseName: "systemPink", promiseColor: "systemPink")
         ]),
-        Day(day: "WED  \n 3", promise: [
-            Promise(promiseName: "blue", promiseColor: "blue"),
+        Day(day: 2, promise: [
+            Promise(promiseName: "purple", promiseColor: "purple"),
             Promise(promiseName: "green", promiseColor: "green"),
-            Promise(promiseName: "blue", promiseColor: "blue")
+            Promise(promiseName: "systemPink", promiseColor: "systemPink")
         ])
     ]
+    
+//    var days: [Day] = [
+//        Day(day: "MON  \n 1", promise: [
+//            Promise(promiseName: "red", promiseColor: "red"),
+//            Promise(promiseName: "yellow", promiseColor: "yellow")
+//        ]),
+//        Day(day: "TUE  \n 2", promise: [
+//            Promise(promiseName: "yellow", promiseColor: "yellow"),
+//            Promise(promiseName: "green", promiseColor: "green"),
+//            Promise(promiseName: "blue", promiseColor: "blue"),
+//            Promise(promiseName: "brown", promiseColor: "brown"),
+//            Promise(promiseName: "blue", promiseColor: "blue"),
+//            Promise(promiseName: "green", promiseColor: "green"),
+//            Promise(promiseName: "blue", promiseColor: "blue")
+//        ]),
+//        Day(day: "WED  \n 3", promise: [
+//            Promise(promiseName: "blue", promiseColor: "blue"),
+//            Promise(promiseName: "green", promiseColor: "green"),
+//            Promise(promiseName: "blue", promiseColor: "blue")
+//        ])
+//    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.dayTableView.delegate = self
         self.dayTableView.dataSource = self
     
-//        dayTableView.estimatedRowHeight = 1000
-//        dayTableView.rowHeight = UITableView.automaticDimension
-//        dayTableView.separatorColor = .clear
-//        dayTableView.reloadData()
     }
 
 }
@@ -60,10 +77,6 @@ class DayChildVC: UIViewController {
 
 extension DayChildVC: UITableViewDataSource {
 
-//    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return UITableView.automaticDimension
-//    }
-//
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.days.count
     }
@@ -74,14 +87,6 @@ extension DayChildVC: UITableViewDataSource {
         cell.setPromise(day: self.days[indexPath.row])
         return cell
         
-//        var cell = UITableViewCell()
-//        if let dayCell = dayTableView.dequeueReusableCell(withIdentifier: "DayTVC", for: indexPath) as? DayTVC {
-//            cell.backgroundColor = .brown
-//            dayCell.setPromise(day: self.days[indexPath.row])
-//            cell = dayCell
-//        }
-//
-//        return cell
     }
     
 }
@@ -94,40 +99,3 @@ extension DayChildVC: UITableViewDelegate {
     }
 }
 
-
-//let rowData = self.days[indexPath.row]
-                    
-//                dayCell.dayLabel.text = rowData.date
-//                dayCell.promises = rowData.promiseList
-
-//                print(dayCell.promises)
-//                dayCell.updateStackView()
-                
-//                for (index, element) in rowData.promiseList.enumerated(){
-//                    //            print(index)
-////                    print(promises[index])
-//                    let view = UIView()
-//                    let label = UILabel()
-//
-//                    // 안먹음
-//                    view.addSubview(label)
-//                    label.text = element.title
-//
-//                    view.backgroundColor = element.color
-//
-//                    // 안먹음
-//                    var f = view.frame
-//                    f.size = CGSize(width: 30, height: 10)
-//                    //            view.frame.size.height = CGFloat(10.0)
-//                    view.frame = f
-////                    print(view.frame.size.width)
-////                    print(view.frame.size.height)
-//                    dayCell.promiseView.addArrangedSubview(view)
-//                    print(dayCell.promiseView.arrangedSubviews[0].frame.size)
-
-//                    dayCell.promiseView.arrangedSubviews[index].widthAnchor.constraint(equalToConstant: dayCell.promiseView.frame.width).isActive = true
-//                    dayCell.promiseView.arrangedSubviews[index].heightAnchor.constraint(equalToConstant: 20.0).isActive = true
-//                    print(dayCell.promiseView.arrangedSubviews[0].frame.size)
-                    
-//                }
-                
--- PinkyPromise/Views/MyTab/DayTVC.swift
+++ PinkyPromise/Views/MyTab/DayTVC.swift
@@ -13,40 +13,33 @@ class DayTVC: UITableViewCell {
     @IBOutlet weak var dayLabel: UILabel!
     @IBOutlet weak var promiseView: UIStackView!
     
+    
+    
 //    fileprivate let promiseHeight: CGFloat = 30
     
     func setPromise(day: Day){
+        
+        let date = Date(timeIntervalSinceNow: TimeInterval(day.day*86400))
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en")
+        dateFormatter.dateFormat = "EEEEEEE\nd"
+        
+        
+        
         DispatchQueue.main.async {
-            self.dayLabel.text = day.day
+            
+            self.dayLabel.text = dateFormatter.string(from: date)
+            if(day.day == 0) {
+                
+                self.dayLabel.layer.masksToBounds = true
+                self.dayLabel.layer.cornerRadius = self.dayLabel.layer.frame.height/2
+                self.dayLabel.backgroundColor = UIColor.purple.withAlphaComponent(0.2)
+
+            }
             
             if self.promiseView.arrangedSubviews.count != day.promise.count {
                 for promise in day.promise {
                     let viewToAdd = OnePromiseView(frame: CGRect.zero, promise: promise)
-//                    viewToAdd.backgroundColor = UIColor(named: "blue")
-//                    viewToAdd.backgroundColor = UIColor(named: promise.promiseColor)
-//                    viewToAdd.backgroundColor = UIColor.blue
-//                    print(viewToAdd.backgroundColor)
-//                    if promise != nil {
-//
-//                        let unwrappedPromise = promise
-//                        //            let backgroundColor = "UIColor" + unwrappedPromise.promiseColor
-////                        print(unwrappedPromise.promiseName)
-//                        let selector = Selector("redColor")
-//                        //            print("\(unwrappedPromise.promiseName)Color")
-//                        if UIColor.self.responds(to: selector) {
-//                            let color = UIColor.self.perform(selector).takeUnretainedValue()
-//                            //                return (color as! UIColor)
-//                            print(color)
-//                            viewToAdd.backgroundColor = color as? UIColor
-//                        } else {
-//                            print("fail")
-//                        }
-//                        //            print(backgroundColor)
-//                    }
-
-                    
-//                    print(promise.promiseColor)
-//                    print(promise.promiseName)
                     self.promiseView.addArrangedSubview(viewToAdd)
                 }
             }
@@ -55,15 +48,14 @@ class DayTVC: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-//        promiseView.backgroundColor = .lightGray
         dayLabel.makeTwoLine()
+//        dayLabel.lineBreakMode = .byWordWrapping
+//        dayLabel.numberOfLines = 2
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
 
 }
