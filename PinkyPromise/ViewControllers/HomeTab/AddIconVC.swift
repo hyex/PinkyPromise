db91bb14b02f7c9d34b400ee445e28323e36ee5e
Uijeong97 <uiui_97@naver.com>
2020-01-28 23:14:42 +0900

    test

--- PinkyPromise/ViewControllers/HomeTab/AddIconVC.swift
+++ PinkyPromise/ViewControllers/HomeTab/AddIconVC.swift
@@ -83,7 +83,7 @@ extension AddIconVC: UICollectionViewDelegate, UICollectionViewDataSource {
         return CGSize(width: 55, height: 55)
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let cell = collectionView.cellForItem(at: indexPath) as! ColorCVC
+        let cell = collectionView.cellForItem(at: indexPath) as! IconCVC
         cell.layer.borderColor = UIColor.gray.cgColor
         
 //        cell.getButtonColor()
