ef693c17f8d13b0c6d131c5f58bc750e3a344213
odyflame <ieehyeon@naver.com>
2020-02-03 01:24:00 +0900

    finallygit statusgit statusgit status success to make homeTab Api. i'm crying now...

--- PinkyPromise/Services/Api.swift
+++ PinkyPromise/Services/Api.swift
@@ -25,15 +25,15 @@ class MyApi: NSObject {
     
     let dateFormatter = DateFormatter()
     
-    func fireStoreSetting() {
-        let store = Firestore.firestore()
-        
-        let setting = FirestoreSettings()
-        setting.isPersistenceEnabled = true
-        setting.cacheSizeBytes = FirestoreCacheSizeUnlimited
-        
-        store.settings = setting
-    }
+//    func fireStoreSetting() {
+//        let store = Firestore.firestore()
+//
+//        let setting = FirestoreSettings()
+//        setting.isPersistenceEnabled = true
+//        setting.cacheSizeBytes = FirestoreCacheSizeUnlimited
+//
+//        store.settings = setting
+//    }
     
     // Api 예시
     func allMore(completion: ([MoreTableData]) -> Void) { //}, onError: @escaping (Error) -> Void) {
@@ -520,28 +520,6 @@ class MyApi: NSObject {
                     debugPrint(err.localizedDescription)
                 } else {
                     
-//                    func a() {
-//                        a { r1, r2, r3 in
-//
-//                        }
-//                    }
-//
-//                    func b() {
-//
-//                        getUserfirebase { snapshot
-//                            let users = snapshot
-//                            getPromise  { sn2
-//
-//                                let promises = sn2
-//                                getProgress { sn3
-//                                    let progress = sn3
-//                                    completion(sn1, sn2, sn3)
-//                                }
-//                            }
-//                        }
-//                    }
-                    
-                    
                     let tempResult1 = PromiseTable.parseData(snapShot: snapShot)
                     
                     let tempResult2 = tempResult1.filter { $0.promiseStartTime.timeIntervalSince1970 <= firstDayPlusi.timeIntervalSince1970 && $0.promiseEndTime.timeIntervalSince1970 >= firstDayPlusi.timeIntervalSince1970 }
@@ -621,24 +599,146 @@ class MyApi: NSObject {
         return days
     }
     
-    //homeTab
-    func getAllDataWithCurrentMonth (completion: @escaping ([PromiseAndProgress]) -> Void) {
-        let Days = self.getTotalDate()
-        let tempResult = [PromiseAndProgress]()
-       
+    //real homeTab
+    func getAllHome(completion: @escaping ([PromiseAndProgress1]) -> Void){
+        let days = self.getTotalDate()//이번 달의 날짜 수 31일
+        
+        let cal = Calendar.current
+        let components = cal.dateComponents([.year, .month, .day, .weekday, .hour, .minute], from: Date())
+        
+        let todayDay = components.day // 오늘이 며칠째인지
+        
+        let firstDay = Date(timeIntervalSince1970: Date().timeIntervalSince1970 - Double(86400 * (todayDay! - 1)) + 32400)//이번달의 첫번째 날
+        
+        let firstDayPlusi = Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(1 * 86400) )
+        
+        var temp3 = [PromiseAndProgress1]()
+        
+        self.getAllDataWithDate(day: firstDay) { (result) in
+            temp3.append(result)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(1 * 86400) ) ) { (result2) in
+                temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(2 * 86400) ) ) { (result2) in
+                    temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(3 * 86400)) ) { (result2) in
+                        temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(4 * 86400) ) ) { (result2) in
+                            temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(5 * 86400)) ) { (result2) in
+                                temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(6 * 86400) ) ) { (result2) in
+                                    temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(7 * 86400)) ) { (result2) in
+                                        temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(8 * 86400) ) ) { (result2) in
+                                            temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(9 * 86400) ) ) { (result2) in
+                                                temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(10 * 86400) ) ) { (result2) in
+                                                    temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(11 * 86400)) ) { (result2) in
+                                                        temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(12 * 86400) ) ) { (result2) in
+                                                            temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(13 * 86400) ) ) { (result2) in
+                                                                temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(14 * 86400)) ) { (result2) in
+                                                                    temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(15 * 86400)) ) { (result2) in
+                                                                        temp3.append(result2)
+         self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(16 * 86400) ) ) { (result2) in
+                                                                            temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(17 * 86400)) ) { (result2) in
+             temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(18 * 86400)) ) { (result2) in
+                         temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(19 * 86400) ) ) { (result2) in
+         temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(20 * 86400)) ) { (result2) in
+              temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(21 * 86400) ) ) { (result2) in
+            temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(22 * 86400) ) ) { (result2) in
+             temp3.append(result2)
+                                                                                    
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(23 * 86400)) ) { (result2) in
+            temp3.append(result2)                         
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(24 * 86400) ) ) { (result2) in
+            temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(25 * 86400) ) ) { (result2) in
+             temp3.append(result2)
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(26 * 86400) ) ) { (result2) in
+            temp3.append(result2)
+                                                                                                    
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(27 * 86400) ) ) { (result2) in
+            temp3.append(result2)
+                                                             
+        self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(28 * 86400) ) ) { (result2) in
+        temp3.append(result2)
+                    
+            if days > 29 {
+            
+       self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(29 * 86400) ) ) { (result2) in
+                temp3.append(result2)
+        if days > 30 {
+            
+            self.getAllDataWithDate(day: Date(timeIntervalSince1970: firstDay.timeIntervalSince1970 + Double(30 * 86400) ) ) { (result2) in
+                temp3.append(result2)
+                
+                completion(temp3)
+            }
+        } else {
+            completion(temp3)
+        }}
+            } else {
+                completion(temp3)
+            }
+            
+            }}}}}}}}}}}}}}}}}}}}} }}}}}}}}
+    }
+    
+    
+    func getAllDataWithDate(day: Date, completion: @escaping (PromiseAndProgress1) -> Void) {
+        
         promiseCollectionRef.whereField(PROMISEUSERS, arrayContains: FirebaseUserService.currentUserID).getDocuments { (snapShot, error) in
             if let err = error {
                 print(err.localizedDescription)
             } else {
                 let promises = PromiseTable.parseData(snapShot: snapShot)
                 
-                let promises2 = promises.filter { $0.promiseEndTime.timeIntervalSince1970 > Date().timeIntervalSince1970 && $0.promiseStartTime.timeIntervalSince1970 < Date().timeIntervalSince1970 }
+                let promises2 = promises.filter { $0.promiseEndTime.timeIntervalSince1970 >= day.timeIntervalSince1970 && $0.promiseStartTime.timeIntervalSince1970 <= day.timeIntervalSince1970 }
+                
+                var temp = [PromiseAndProgressNotDay]()
+                
+                for douc in promises2 {
+                    
+                    self.progressCollectionRef.whereField(USERID, isEqualTo: FirebaseUserService.currentUserID).whereField(PROMISEID, isEqualTo: douc.promiseId).getDocuments { (snapShot, error) in
+                        if let err = error {
+                            print(err.localizedDescription)
+                        } else {
+                            let progress1 = ProgressTable.parseData(snapShot: snapShot)
+                            let temp2 = PromiseAndProgressNotDay(promiseData: douc, progressData: progress1[0])
+                            temp.append(temp2)
+                            
+                            if temp.count == promises2.count {
+                                completion(PromiseAndProgress1(Day: day, PAPD: temp))
+                            }
+                        }
+                    }
+                    
+                }
+                if promises2.count == 0 {
+                    completion(PromiseAndProgress1(Day: day, PAPD: temp))
+                }
             }
         }
     }
     
     //약속 데이터를 추가할 때 사용하는 함수
     func addPromiseData(_ promiseTable: PromiseTable) {
+        
+        var temp = promiseTable.promiseUsers
+        temp.append(FirebaseUserService.currentUserID!)
         
         promiseCollectionRef.document(promiseTable.promiseId).setData([
             PROMISENAME : promiseTable.promiseName ?? "Anomynous",
@@ -648,7 +748,7 @@ class MyApi: NSObject {
             PROMISESTARTTIME: promiseTable.promiseStartTime ,
             PROMISEENDTIME: promiseTable.promiseEndTime ,
             PROMSISEPANALTY: promiseTable.promisePanalty ?? "nil",
-            PROMISEUSERS: promiseTable.promiseUsers ,
+            PROMISEUSERS: temp ,
             PROMISEID: promiseTable.promiseId ?? "nil"
         ]) { error in
             if let err = error {
@@ -676,6 +776,7 @@ class MyApi: NSObject {
                 print("this is API success")
             }
         }
+        
     }
     
     //프로그레스테이블에 데이터 추가, addPromiseTable 직후 같은 promiseTable을 넣어준다.
@@ -786,6 +887,25 @@ class MyApi: NSObject {
         
     }
     
+    //프로그레스 입력뷰
+    func updateProgress(day: Date, userId: String, promiseId: String, progress: ProgressTable ){
+        progressCollectionRef.whereField(USERID, isEqualTo: userId).whereField(PROMISEID, isEqualTo: promiseId).getDocuments { (snapShot, error) in
+            if let err = error {
+                debugPrint(err.localizedDescription)
+            } else {
+                self.progressCollectionRef.document((snapShot?.documents[0].documentID)!).updateData([PROGRESSDEGREE : progress]) { err in
+                    if let err = err {
+                        print("Error updating document: \(err)")
+                    } else {
+                        print("Document successfully updated")
+                    }
+                    
+                }
+            }
+        }
+    }
+    
+    
     func randomNonceString(length: Int = 32) -> String {
         precondition(length > 0)
         let charset: Array<Character> =
@@ -831,4 +951,3 @@ class MyApi: NSObject {
     
     
 }
-
