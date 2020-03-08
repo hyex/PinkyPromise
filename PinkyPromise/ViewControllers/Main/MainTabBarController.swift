845165949b88cb024c80c0ca3a08feb2a4989990
hyex <hyexkim@gmail.com>
2020-03-08 22:19:39 +0900

    fix signUp UI

--- PinkyPromise/ViewControllers/Main/MainTabBarController.swift
+++ PinkyPromise/ViewControllers/Main/MainTabBarController.swift
@@ -156,7 +156,7 @@ class MainTabBarController: UITabBarController {
 //                           print(douc2.progressData.promiseId)
 //                           print(douc2.promiseData.promiseName)
 //                       }
-//                   }
+//                    }
 //               }
 
     }
