44c3bac8273873c3d24b1129b9ae199e0a0dec75
Uijeong97 <uiui_97@naver.com>
2020-03-27 20:57:30 +0900

    data Async

--- PinkyPromise/ViewControllers/HomeTab/AddPromiseVC.swift
+++ PinkyPromise/ViewControllers/HomeTab/AddPromiseVC.swift
@@ -375,7 +375,6 @@ extension AddPromiseVC: UITextFieldDelegate {
         guard let text = textField.text else { return true }
         let newLength = text.count + string.count - range.length
         return newLength <= 30
-        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -385,6 +384,7 @@ extension AddPromiseVC: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
+        return true
     }
     // Called just before UITextField is edited
     func textFieldDidBeginEditing(_ textField: UITextField) {
